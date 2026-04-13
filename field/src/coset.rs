dd70a29f2e6df1cdb3bdb4d2007972f8f02d44f6
Ubuntu <ubuntu@ip-10-20-100-196.us-west-2.compute.internal>
2026-04-13 17:10:40 +0000

    fix: use serde bound="" for TwoAdicMultiplicativeCoset

--- field/src/coset.rs
+++ field/src/coset.rs
@@ -52,8 +52,7 @@ use crate::{BoundedPowers, TwoAdicField};
 /// );
 /// ```
 #[derive(Clone, Copy, Debug, serde::Serialize, serde::Deserialize)]
-#[serde(bound(serialize = "F: serde::Serialize"))]
-#[serde(bound(deserialize = "F: serde::Deserialize<'de>"))]
+#[serde(bound = "")]
 pub struct TwoAdicMultiplicativeCoset<F: TwoAdicField> {
     // Letting s = shift, and g = generator (of order 2^log_size), the coset in
     // question is
