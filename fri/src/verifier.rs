e13292255f62d6511d28cbe246044a53b5c5b09e
Ubuntu <ubuntu@ip-10-20-100-196.us-west-2.compute.internal>
2026-04-14 23:02:12 +0000

    fix: remove log_arity observations from FRI transcript to match faa24ca protocol

--- fri/src/verifier.rs
+++ fri/src/verifier.rs
@@ -230,11 +230,6 @@ where
         });
     }
 
-    // Bind the variable-arity schedule into the transcript before query grinding.
-    for &log_arity in &log_arities {
-        challenger.observe(Val::from_usize(log_arity));
-    }
-
     // Check PoW.
     if !challenger.check_witness(params.query_proof_of_work_bits, proof.query_pow_witness) {
         return Err(FriError::InvalidPowWitness);
